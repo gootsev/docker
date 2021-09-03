cc98f8bf15d2f212bf0ace1ae5a5bca69274ecb7
gootsev <gootsev@gmail.com>
2021-09-03 16:05:29 +0300

    tabs to spaces

--- app.py
+++ app.py
@@ -12,29 +12,29 @@ api = os.environ['API_KEY']
 
 @app.route('/')
 def main_app():
-	params = {'q': {city}, 'appid': {api}}
-	request = requests.get("https://api.openweathermap.org/data/2.5/weather?units=metric", params=params)
-	description = request.json()["weather"][0]["description"]
-	temp = request.json()['main']['temp']
-	db_connect(temp)
-	return f"Hi {name}! It's {temp} degrees in {city} now, {description}."
+    params = {'q': {city}, 'appid': {api}}
+    request = requests.get("https://api.openweathermap.org/data/2.5/weather?units=metric", params=params)
+    description = request.json()["weather"][0]["description"]
+    temp = request.json()['main']['temp']
+    db_connect(temp)
+    return f"Hi {name}! It's {temp} degrees in {city} now, {description}."
 
 
 def db_connect(temp):
-	connection = psycopg2.connect(user='postgres', password='postgres', database='postgres', host="db")
-	try:
-		cursor = connection.cursor()
-		cursor.execute('CREATE TABLE IF NOT EXISTS weather (id SERIAL PRIMARY KEY, VALUE VARCHAR(255) NOT NULL)')
-		sql = f"INSERT INTO weather (value) VALUES ( {temp} )"
-		cursor.execute(sql)
-		connection.commit()
-	except (Exception, psycopg2.DatabaseError) as error:
-		print("Error while connecting to PostgreSQL", error)
-	finally:
-		if(connection):
-			cursor.close()
-			connection.close()
+    connection = psycopg2.connect(user='postgres', password='postgres', database='postgres', host="db")
+    try:
+        cursor = connection.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS weather (id SERIAL PRIMARY KEY, VALUE VARCHAR(255) NOT NULL)')
+        sql = f"INSERT INTO weather (value) VALUES ( {temp} )"
+        cursor.execute(sql)
+        connection.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if(connection):
+            cursor.close()
+            connection.close()
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
